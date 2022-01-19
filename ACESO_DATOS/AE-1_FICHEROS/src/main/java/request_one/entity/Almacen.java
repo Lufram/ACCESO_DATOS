87ce337802684be5291977392f2576792fd440b6
Lufram <63366850+Lufram@users.noreply.github.com>
2022-01-19 16:09:57 +0100

    Update Almacen.java

--- ACESO_DATOS/AE-1_FICHEROS/src/main/java/request_one/entity/Almacen.java
+++ ACESO_DATOS/AE-1_FICHEROS/src/main/java/request_one/entity/Almacen.java
@@ -3,6 +3,9 @@ package main.java.request_one.entity;
 import java.io.*;
 import java.util.ArrayList;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class Almacen {
 	
 	private static String NOMBRE_FICHERO;
@@ -96,38 +99,51 @@ public class Almacen {
 		}
 	}
 
+	/**
+	* 
+	*
+	* 
+	**/
+	public void initFile() {
+        
+		try (FileReader fr = new FileReader(NOMBRE_FICHERO);
+		BufferedReader br = new BufferedReader(fr);) {
 
-    public void initFile() {
-        Gson gson = new Gson();
-        try (FileReader fr = new FileReader(getNombreFichero());
-             BufferedReader br = new BufferedReader(fr);) {
+		String json = br.readLine();
+		Gson gson = new Gson();
+		List<Coche> stock = gson.fromJson(json, new TypeToken<List<Coche>>(){}.getType());
+			//Persona p = gson.fromJson(json, Persona.class);
+			for(Coche c : stock) {
+				addItem(c);
+			}
+		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-            String json = gson.toJson();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+	}
+	
+	/**
+	* 
+	*
+	* 
+	**/
+	public void updateFile() {
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(stock);
+	    
+		try(FileWriter fw = new FileWriter(NOMBRE_FICHERO)){
+			
+			fw.write(json);
+			System.out.println("Fichero creado");
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    }
-
-    public String updateFile() {
-
-        Gson gson = new Gson();
-        String json = gson.toJson(getStock());
-
-        try(FileWriter fw = new FileWriter(getNombreFichero())){
-            fw.write(json);
-            return "INFORMACION GUARDADA";
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return "ERROR AL ACEDER AL FICHERO");
-        }
-
-    }
+	}
 }
 
